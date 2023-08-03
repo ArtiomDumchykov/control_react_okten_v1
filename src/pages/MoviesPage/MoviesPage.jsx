3b72e6f4c00610440d0d5f001bb2ba37811552b5
ArtiomDumchykov <66011687+ArtiomDumchykov@users.noreply.github.com>
2023-08-03 16:42:27 +0300

    fix

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,21 +1,24 @@
-import React, {useState } from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 import { getSearchParams } from '../../utils';
 import { movieService } from '../../services';
 import { MovieFilter, MoviesLists } from '../../components';
+import { useFilterForm } from '../../hooks';
 
 export function MoviesPage() {
 
     let params = useParams();
+    const [searchParams, setSearchParams] = useState({...params, ...getSearchParams(params)})
 
-    const [selectDataForm, setSelectDataForm] = useState({ ...params,  ...getSearchParams(params) });
+    // const {} = useFilterForm({params, getSearchParams})
 
     return (
         <>
 
-            <MovieFilter dataForm={{ selectDataForm, setSelectDataForm }} />
-            <MoviesLists callback={movieService.getAll} params={{ ...selectDataForm}} />
+            {/* <MovieFilter dataForm={{ register, handleSubmit, setSearchParams, handleSearchForm,  setSearchParams}} /> */}
+            <MoviesLists callback={movieService.getAll} params={{ ...searchParams }} />
         </>
     );
 }
+
